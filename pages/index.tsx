6f22ba761938c2023d924f6e2c9573251c60c9f2
thoerni <maxpaul.thorn@gmail.com>
2023-04-13 23:44:10 +0200

    MAXIMALE RESPONSIVENESSgit add .

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,43 +25,66 @@ export default function Home() {
         </div>
         <div>
           <div className="step">
-            <div className="small-title-row">
-              <h2 className="small-title">Schritt 1</h2>
-              <div className="small-title-icon">
-              <Image src={DownloadIcon} alt="Download" height={80}></Image>
+            <div className="step-row">
+              <div className='step-row-text'>
+                <div className="small-title-row">
+                  <h2 className="small-title">Schritt 1</h2>
+                  <div className="small-title-icon">
+                    <Image src={DownloadIcon} alt="Download" height={80}></Image>
+                  </div>
+                </div>
+                <div className='step-description'>
+                  Installiere dir die{' '}
+                  <a href="https://www.ausweisapp.bund.de/download">AusweisApp2</a> auf dein Handy oder besorge dir einen kompatiblen{' '}
+                  <a href="https://www.ausweisapp.bund.de/usb-kartenleser">USB-Kartenleser</a> und scanne deine{' '}
+                  <a href="https://www.personalausweisportal.de/Webs/PA/EN/citizens/german-id-card/german-id-card-node.html">ID-Karte</a>.
+                </div>
               </div>
-            </div>
-            <div className='step-description'>
-              Installiere dir die{' '}
-              <a href="https://www.ausweisapp.bund.de/download">AusweisApp2</a> auf dein Handy oder besorge dir einen kompatiblen{' '}
-              <a href="https://www.ausweisapp.bund.de/usb-kartenleser">USB-Kartenleser</a> und scanne deine{' '}
-              <a href="https://www.personalausweisportal.de/Webs/PA/EN/citizens/german-id-card/german-id-card-node.html">ID-Karte</a>.
+              <div className="big-title-icon">
+                <Image src={DownloadIcon} alt="Download" height={130}></Image>
+              </div>
             </div>
           </div>
           <div className="step">
-            <div className="small-title-row">
-              <h2 className="small-title">Schritt 2</h2>
-              <div className="small-title-icon">
-                <Image src={SecureIcon} alt="Download" height={80}></Image>
+            <div className="step-row">
+              <div className="big-title-icon">
+                <Image src={SecureIcon} alt="Download" height={130}></Image>
               </div>
-            </div>
-            <div className='step-description'>
-              Gib deine 6-stellige eID-PIN in ein.{' '}
-              <a href="https://www.bundesdruckerei.de/de/newsroom/pressemitteilungen/neuer-pin-fuer-den-online-ausweis">Noch keine PIN?</a>
+              <div className='step-row-text'>
+                <div className="small-title-row">
+                  <h2 className="small-title">Schritt 2</h2>
+                  <div className="small-title-icon">
+                    <Image src={SecureIcon} alt="Download" height={80}></Image>
+                  </div>
+                </div>
+                <div className='step-description'>
+                  Gib deine 6-stellige eID-PIN in ein.{' '}
+                  <a href="https://www.bundesdruckerei.de/de/newsroom/pressemitteilungen/neuer-pin-fuer-den-online-ausweis">Noch keine PIN?</a>
+                </div>
+              </div>
             </div>
           </div>
           <div className="step">
-            <div className="small-title-row">
-            <h2 className="small-title">Schritt 3</h2>
-              <div className="small-title-icon">
-                <Image src={BankIcon} alt="Download" height={80}></Image>
+            <div className="step-row">
+              <div className='step-row-text'>
+                <div className="small-title-row">
+                  <h2 className="small-title">Schritt 3</h2>
+                  <div className="small-title-icon">
+                    <Image src={BankIcon} alt="Download" height={80}></Image>
+                  </div>
+                </div>
+                <div className='step-description'>
+                  Gib deine Bankdaten an und bekomme dein Geld in wenigen Tagen überwiesen.
+                </div>
+              </div>
+              <div className="big-title-icon">
+                <Image src={BankIcon} alt="Download" height={130}></Image>
               </div>
             </div>
-            <div className='step-description'>
-              Gib deine Bankdaten an und bekomme dein Geld in wenigen Tagen überwiesen.
-            </div>
           </div>
-          <button className="btn">Hol dir jetzt dein Gelb</button>
+          <div className="btnrow">
+           <button className="btn">Hol dir jetzt dein Gelb</button>
+          </div>
         </div>
       <ErrorMSG/>
       <IBANInput/>
