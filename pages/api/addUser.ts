22b1e28201a5fdbffa8fe08d024222104de470d2
niels <33730531+yndolg@users.noreply.github.com>
2023-04-14 05:43:17 +0200

    backend Co-authored-by: Aleksandrs Morgensterns <Axelotl35@users.noreply.github.com>

--- pages/api/addUser.ts
+++ pages/api/addUser.ts
@@ -1,9 +1,23 @@
-export default function handler(req, res) {
-    const { method, body } = req;
+import {prisma} from "@/lib/database";
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {hash} from "@lib/secureHashTM"
+type Data = {
+  name: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+  ) {    const { method, body } = req;
 
     if (method === 'POST' || method === 'GET') {
-        console.log('Request payload:', body)
-        res.status(200).json({message: "success"})
+        const user = await prisma.enrolment.create({
+            data: {
+              token: body.name
+            },
+          });
+        res.redirect("/test");
     } else {
         res.status(405).json({ message: 'Method not allowed' })
     }
