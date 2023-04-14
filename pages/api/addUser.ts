fbdeddbf42b49e08d662f5ae7ab94d205e8f64a4
roehrt <roehrt@users.noreply.github.com>
2023-04-14 11:07:09 +0200

    Refactor

--- pages/api/addUser.ts
+++ pages/api/addUser.ts
@@ -1,25 +1,26 @@
-import {prisma} from "@/lib/database";
+import { prisma } from "@/lib/database";
 
 import type { NextApiRequest, NextApiResponse } from 'next'
-import hash from "@/lib/secureHashTM";
+import secureHash from "@/lib/secureHashTM";
 
 type Data = {
   name: string
 }
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
-  ) {    const { method, body } = req;
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { method, body } = req;
 
-    if (method === 'POST' || method === 'GET') {
-        const user = await prisma.enrolment.create({
-            data: {
-              token: hash(body),
-            },
-          });
-        res.redirect("/test");
-    } else {
-        res.status(405).json({ message: 'Method not allowed' })
-    }
+  if (method === 'POST' || method === 'GET') {
+    await prisma.enrolment.create({
+      data: {
+        token: secureHash(body),
+      },
+    });
+    res.redirect("/test");
+  } else {
+    res.status(405).json({ message: 'Method not allowed' })
+  }
 }
